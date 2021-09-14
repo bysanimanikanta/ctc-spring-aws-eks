6a78b01039a238d346296af70706ff2aad442a6e
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-14 10:27:04 -0500

    Disable Spring Security

--- src/main/java/com/esrx/pubcloud/springawseks/SpringAwsEksApplication.java
+++ src/main/java/com/esrx/pubcloud/springawseks/SpringAwsEksApplication.java
@@ -1,9 +1,10 @@
 package com.esrx.pubcloud.springawseks;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ManagementWebSecurityAutoConfiguration.class})
 public class SpringAwsEksApplication {
 
 	public static void main(String[] args) {
