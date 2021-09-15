e18b713be6e85031895bef4e7d694213e9875e4f
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-14 22:29:00 -0500

    Include environment variable

--- src/main/java/com/esrx/pubcloud/springawseks/controller/Greetings.java
+++ src/main/java/com/esrx/pubcloud/springawseks/controller/Greetings.java
@@ -1,5 +1,8 @@
 package com.esrx.pubcloud.springawseks.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/labday/")
 public class Greetings {
 
+    @Value("${MESSAGE_FROM: DEFAULT}")
+    private String messageFrom;
+
+    @Autowired
+    BuildProperties  buildProperties;
+
     @RequestMapping("greet")
     public String greet() {
-        return "Hey!! This is CTC Lab day...";
+        return "Message from " + messageFrom + ": Hello, this is CTC Lab day and application version is " +
+                buildProperties.getArtifact() + "-" + buildProperties.getVersion();
     }
 }
