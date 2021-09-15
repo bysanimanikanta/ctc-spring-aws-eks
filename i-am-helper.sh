bda11c0f30c967d9d8b2c08d4225a48a9a31e07f
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-15 16:03:33 -0500

    Conver uppwer case to lower case

--- i-am-helper.sh
+++ i-am-helper.sh
@@ -9,6 +9,7 @@ if [ -z "$LAN_ID" ]; then
 	exit 1
 fi
 
-echo "Replacing all the k8s object files with $LAN_ID as namespaces"
-sed -i "s/REPLACEME/$LAN_ID/g" k8s/*.yaml
+LAN_ID_LOWER=$(echo $LAN_ID | tr "[:upper:]" "[:lower:]")
+echo "Replacing all the k8s object files with $LAN_ID_LOWER as namespaces"
+sed -i "s/REPLACEME/$LAN_ID_LOWER/g" k8s/*.yaml
 echo "Done"
