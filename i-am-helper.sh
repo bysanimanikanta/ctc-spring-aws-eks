0ffd76a198a262c11a3700c1918f3a12d89c1876
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-15 14:44:05 -0500

    Remove docker helper script

--- i-am-helper.sh
+++ i-am-helper.sh
@@ -12,15 +12,3 @@ fi
 echo "Replacing all the k8s object files with $LAN_ID as namespaces"
 sed -i "s/REPLACEME/$LAN_ID/g" k8s/*.yaml
 echo "Done"
-
-echo "Please enter your docker image URL:"
-read DOCKER_IMAGE_URL
-
-if [ -z "$DOCKER_IMAGE_URL" ]; then
-	echo "Do not try to break me!!! Please enter your docker image URL"
-	exit 1
-fi
-
-echo "Replacing docker image URL in deployment.yaml"
-sed -i "s/REPLACEDOCKERIMAGE/$DOCKER_IMAGE_URL/g" k8s/deployment.yaml
-echo "Done"
