f0a972f8ff5a8edcbe1d0ebc7327d0766d31b208
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-15 14:15:58 -0500

    Update helper script to replace docker image url

---
+++ i-am-helper.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+set -e
+
+echo "Please enter your LanID to create K8s objects in your namespace"
+read LAN_ID
+
+if [ -z "LAN_ID" ]; then
+	echo "Do not try to break me!!! Please enter your LAN ID"
+	exit 1
+fi
+
+echo "Replacing all the k8s object files with $LAN_ID as namespaces"
+sed -i "s/REPLACEME/$LAN_ID/g" k8s/*.yaml
+echo "Done"
+
+echo "Please enter your docker image URL"
+read DOCKER_IMAGE_URL
+
+if [ -z "DOCKER_IMAGE_URL" ]; then
+	echo "Do not try to break me!!! Please enter your docker image URL"
+	exit 1
+fi
+
+echo "Replacing docker image URL in deployment.yaml"
+sed -i "s/REPLACEDOCKERIMAGE/DOCKER_IMAGE_URL/g" k8s/deployment.yaml
