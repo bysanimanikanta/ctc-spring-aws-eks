b6c9b763a6d1f11251d2514457ebb4ddb680e5df
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-23 14:54:12 -0500

    Update helper script to replace docker image as well

--- i-am-helper.sh
+++ i-am-helper.sh
@@ -9,7 +9,16 @@ if [ -z "$LAN_ID" ]; then
 	exit 1
 fi
 
+echo "Please enter your Docker image URI:"
+read DOCKER_IMAGE_URI
+
+if [ -z "$DOCKER_IMAGE_URI" ]; then
+  echo "Do not try to break me!!! Please enter your docker image uri"
+  exit 1
+fi
+
 LAN_ID_LOWER=$(echo $LAN_ID | tr "[:upper:]" "[:lower:]")
-echo "Replacing all the k8s object files with $LAN_ID_LOWER as namespaces"
+echo "Replacing all the k8s object files with $LAN_ID_LOWER as namespaces and image as $DOCKER_IMAGE_URI"
 sed -i "s/REPLACEME/$LAN_ID_LOWER/g" k8s/*.yaml
+sed -i "s|REPLACEDOCKERIMAGE|$DOCKER_IMAGE_URI|g" k8s/deployment.yaml
 echo "Done"
