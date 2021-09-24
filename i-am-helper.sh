94ea523a19889732ce01e34cc10a9130eb104cd1
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-23 22:48:52 -0500

    Include sed command for hpa files

--- i-am-helper.sh
+++ i-am-helper.sh
@@ -20,5 +20,7 @@ fi
 LAN_ID_LOWER=$(echo $LAN_ID | tr "[:upper:]" "[:lower:]")
 echo "Replacing all the k8s object files with $LAN_ID_LOWER as namespaces and image as $DOCKER_IMAGE_URI"
 sed -i "s/REPLACEME/$LAN_ID_LOWER/g" k8s/*.yaml
+sed -i "s/REPLACEME/$LAN_ID_LOWER/g" k8s/autoscaling/*.yaml
 sed -i "s|REPLACEDOCKERIMAGE|$DOCKER_IMAGE_URI|g" k8s/deployment.yaml
+sed -i "s|REPLACEDOCKERIMAGE|$DOCKER_IMAGE_URI|g" k8s/autoscaling/deployment.yaml
 echo "Done"
