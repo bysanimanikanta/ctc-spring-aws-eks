d08ba2eaaf47ef2f197547c7546ce2d766a2db86
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-26 17:10:08 -0500

    Update helper script to look for only yaml file

--- i-am-helper.sh
+++ i-am-helper.sh
@@ -5,6 +5,6 @@ DOCKER_IMAGE_URI=112834256162.dkr.ecr.us-east-1.amazonaws.com/$LAN_ID/aws-spring
 LAN_ID_LOWER=$(echo $LAN_ID | tr "[:upper:]" "[:lower:]")
 
 echo "Replacing all the k8s object files with $LAN_ID_LOWER as namespaces and image as $DOCKER_IMAGE_URI"
-find . -type f -not -path '*/\.*' -exec sed -i "s/REPLACEME/$LAN_ID_LOWER/g" {} +
-find . -type f -not -path '*/\.*' -exec sed -i "s|REPLACEDOCKERIMAGE|$DOCKER_IMAGE_URI|g" {} +
-echo "Done"
+find . -type f -name "*.yaml" -exec sed -i "s/REPLACEME/$LAN_ID_LOWER/g" {} +
+find . -type f -name "*.yaml" -exec sed -i "s|REPLACEDOCKERIMAGE|$DOCKER_IMAGE_URI|g" {} +
+echo "Done. Do a git diff to see all the changes"
