d3d46f83e46633a4566fda5cb294f9085b5106f8
bysanimanikanta <bysanimanikanta@gmail.com>
2021-09-26 22:39:58 -0500

    Include logger in controller

--- src/main/java/com/esrx/pubcloud/springawseks/controller/Greetings.java
+++ src/main/java/com/esrx/pubcloud/springawseks/controller/Greetings.java
@@ -1,5 +1,7 @@
 package com.esrx.pubcloud.springawseks.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/labday/")
 public class Greetings {
 
+    Logger logger = LoggerFactory.getLogger(Greetings.class);
+
     @Value("${MESSAGE_FROM: DEFAULT}")
     private String messageFrom;
 
@@ -18,6 +22,8 @@ public class Greetings {
 
     @RequestMapping("greetings")
     public String greet() {
+        logger.info("Message from " + messageFrom + ": Hello, this is CTC Lab day and application version is " +
+                buildProperties.getArtifact() + "-" + buildProperties.getVersion());
         return "Message from " + messageFrom + ": Hello, this is CTC Lab day and application version is " +
                 buildProperties.getArtifact() + "-" + buildProperties.getVersion();
     }
